53d346b13a46c85e714d8e71b19d7ff1f18b7c69
Nonoka Yamamoto <garden.n1234567890@gmail.com>
2020-04-11 11:38:48 +0900

    Initial Commit

---
+++ MyJanken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyJanken
+//
+//  Created by 山本ののか on 2020/04/11.
+//  Copyright © 2020 NonokaYamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
